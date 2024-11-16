800510fc5ed0995ac1bea34ed05fb8ce85f0aa80
dmycse <dmycse@gmail.com>
2024-11-16 20:52:10 +0200

    Refactor(components): refactor styles of todo app component

--- src/components/todo_app.tsx
+++ src/components/todo_app.tsx
@@ -15,15 +15,15 @@ export let TodoApp = () => {
         flexDirection: 'column', 
         justifyContent: 'center', 
         alignItems: 'center', 
-        background: '#ebe8e8'
+        background: '#faf7f7'
     }}>
       <Typography 
         variant="h1" 
-        sx={{fontFamily: 'Inter', fontWeight: '100', color: '#753c3c'}}
+        sx={{fontFamily: 'Inter', fontWeight: '100', color: '#753c3c', opacity: '0.2'}}
       >
         todos
       </Typography>
-      <Stack spacing={2} sx={{width: '100%', height: '100%', overflow: 'auto' }}>
+      <Stack sx={{width: '100%', height: '100%', overflow: 'auto' }}>
         <TodoList />
       </Stack>
     </Stack>
