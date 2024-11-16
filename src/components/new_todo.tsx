9680b902adb2e4d2a4df2cb426de9cf6ff6b4f55
dmycse <dmycse@gmail.com>
2024-11-16 20:00:49 +0200

    Feat(components): feat new todo component

--- src/components/new_todo.tsx
+++ src/components/new_todo.tsx
@@ -2,7 +2,7 @@ import { KeyboardEventHandler, MouseEventHandler, useRef } from "react";
 import { ACTION_TYPE, useTodosState } from "../data";
 
 import { Box, Button, Input } from "@mui/material";
-import AddIcon from '@mui/icons-material/Add';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 export let NewTodo = () => {
 
@@ -13,34 +13,40 @@ export let NewTodo = () => {
   let addNewTodo: MouseEventHandler & KeyboardEventHandler = () => {
     if (inputRef.current!.value !== '') {
       dispatch({type: ACTION_TYPE.ADD_TODO, payload: {title: (inputRef.current?.value as string).trim()}});
-      inputRef.current!.value = "";
+      inputRef.current!.value = '';
     } 
     return;
   };
 
-  console.log('state NewTodo', state);
 
   return (
     <Box
-      sx={{padding: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 1}}
+      component='header'
+      sx={{
+        paddingTop: 1, 
+        paddingBottom: 1, 
+        flexGrow: 0, 
+        display: 'flex', 
+        alignItems: 'center'
+      }}
     >
-
+      <Button 
+        type='button'
+        variant="text"
+        onClick={addNewTodo}
+        sx={{padding: '0px'}}
+      >
+        <KeyboardArrowDownIcon sx={{width: '40px', height:'40px', color: '#ccc'}} />
+      </Button>
       <Input 
         type="text" 
-        placeholder="Add todo" 
-        sx={{flexBasis: '50%', fornFamily: 'Inter', fontSize: '18px'}}  
+        placeholder="What needs to be done?"
+        disableUnderline
+        inputProps={{style: {fontFamily: 'Inter', fontSize: '20px', fontWeight: '100', fontStyle: 'italic'}}} 
+        sx={{ flexBasis: '50%'}}  
         inputRef={inputRef}
         onKeyDown={e => e.key === 'Enter' && addNewTodo(e)}
       />
-
-      <Button 
-        type='button'
-        variant="contained"
-        onClick={addNewTodo}
-      >
-        <AddIcon sx={{width: '20px', height:'20px'}} />
-      </Button>
-
     </Box>
   );
 };
